361fd246cbe0e03d54396d4732e92a7e326ed339
Kevin Zhang <kevinzhang@utexas.edu>
2019-01-18 14:51:35 -0600

    Initial Commit

---
+++ tipperTests/tipperTests.swift
@@ -0,0 +1,34 @@
+//
+//  tipperTests.swift
+//  tipperTests
+//
+//  Created by Kevin Zhang on 1/18/19.
+//  Copyright © 2019 Kevin Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import tipper
+
+class tipperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
